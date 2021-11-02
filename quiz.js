c5699bce1fe4bb55540754cf8be28f3f1ecd1fe0
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-02 14:46:21 -0600

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,1 +1,12 @@
-// random
+// Variable To Store HTML Elements
+let questionInputEl1 = document.getElementById("question-input-1").value;
+let questionInputEl2 = document.getElementById("question-input-2").value;
+let questionInputEl3 = document.getElementById("question-input-3").value;
+let questionInputEl4 = document.getElementById("question-input-4").value;
+let answerOutputEl1 = document.getElementById("answer-output-1");
+let answerOutputEl2 = document.getElementById("answer-output-2");
+let answerOutputEl3 = document.getElementById("answer-output-3");
+let answerOutputEl4 = document.getElementById("answer-output-4");
+let scoreEl = document.getElementById("score");
+
+// Event Listener

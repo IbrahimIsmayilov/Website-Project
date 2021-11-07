b1d2cf3f20771e779b164cea94ea857b901b2f6a
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-07 15:28:08 -0700

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,83 +1,211 @@
 // Variable To Store HTML Elements
-let questionInputEl1 = document.getElementById("question-input-1");
-let questionInputEl2 = document.getElementById("question-input-2");
-let questionInputEl3 = document.getElementById("question-input-3");
-let questionInputEl4 = document.getElementById("question-input-4");
-let answerOutputEl1 = document.getElementById("answer-output-1");
-let answerOutputEl2 = document.getElementById("answer-output-2");
-let answerOutputEl3 = document.getElementById("answer-output-3");
-let answerOutputEl4 = document.getElementById("answer-output-4");
-let scorePercentageEl = document.getElementById("score-percentage");
-let scoreFractionEl = document.getElementById("score-fraction");
-let submitBtnEl = document.getElementById("submit-btn");
-let missedQEl = document.getElementById("missed-q");
+let questionEl1 = document.getElementById("questionInput1");
+let questionEl2 = document.getElementById("questionInput2");
+let questionEl3 = document.getElementById("questionInput3");
+let questionEl4 = document.getElementById("questionInput4");
+let answerEl1 = document.getElementById("answerOutput1");
+let answerEl2 = document.getElementById("answerOutput2");
+let answerEl3 = document.getElementById("answerOutput3");
+let answerEl4 = document.getElementById("answerOutput4");
+let scorepEl = document.getElementById("scorePercentage");
+let scorefEl = document.getElementById("scoreFraction");
+let btnEl = document.getElementById("btn");
+let feedbackEl = document.getElementById("feedback");
+let missedQEl = document.getElementById("missedQ");
 
 // Global Variables
-score = 0;
+scoreF = 0;
 
 // Event Listeners
-submitBtnEl.addEventListener("click", results);
+btnEl.addEventListener("click", results);
 
 // Event Function
 function results() {
 
-    let question1 = questionInputEl1.value.toLowerCase();
-    let question2 = questionInputEl2.value.toLowerCase();
-    let question3 = questionInputEl3.value.toLowerCase();
-    let question4 = questionInputEl4.value.toLowerCase();
+    let question1 = questionEl1.value.toLowerCase();
+    let question2 = questionEl2.value.toLowerCase();
+    let question3 = questionEl3.value.toLowerCase();
+    let question4 = questionEl4.value.toLowerCase();
 
-    
     // Check Question 1
-    if (question2 = "") {
-        answerOutputEl2.innerHTML = "Required!";
-    } else if (question1 === "usa" || question1 === "united states" || question1 === "united states of america") {
+    if (question1 === "" ) {
+        answerEl1.innerHTML = "Required!";
+        questionEl1.style.border = "3px solid black";
+    } else if (question1 === "usa" || question1 === "us" || question1 === "united states of america" || question1 === "united states" ) {
+        answerEl1.innerHTML = "Correct!"
         document.getElementById("flag.jpg").style.display = 'none';
-        answerOutputEl1.innerHTML = "Correct!";
-        score ++;
+        questionEl1.style.border = "3px solid green";
+        scoreF++;
     } else {
-        answerOutputEl1.innerHTML = "Incorrect!";
+        answerEl1.innerHTML = "Incorrect!"
+        questionEl1.style.border = "3px solid red";
     }
 
     // Check Question 2
-    if (question2 === "") {
-        answerOutputEl1.innerHTML = "Required!";
-    } else if (question2 === "ww2" || question1 === "world war 2" || question1 === "world war two") {
-        answerOutputEl1.innerHTML = "Correct!";
-        score ++;
+    if (question2 === "" ) {
+        answerEl2.innerHTML = "Required!";
+        questionEl2.style.border = "3px solid black";
+    } else if (question2 === "ww2" || question2 === "world war two" || question2 === "world war 2") {
+        answerEl2.innerHTML = "Correct!"
+        questionEl2.style.border = "3px solid green";
+        scoreF++;
     } else {
-        answerOutputEl1.innerHTML = "Incorrect!";
+        answerEl2.innerHTML = "Incorrect!"
+        questionEl2.style.border = "3px solid red";
     }
 
     // Check Question 3
-    if (question3 === "") {
-        answerOutputEl3.innerHTML = "Required!";
+    if (question3 === "" ) {
+        answerEl3.innerHTML = "Required!";
+        questionEl3.style.border = "3px solid black";
     } else if (question3 === "1") {
-        answerOutputEl1.innerHTML = "Correct!";
-        score ++;
+        answerEl3.innerHTML = "Correct!"
+        questionEl3.style.border = "3px solid green";
+        scoreF++;
     } else {
-        answerOutputEl1.innerHTML = "Incorrect!";
+        answerEl3.innerHTML = "Incorrect!"
+        questionEl3.style.border = "3px solid red";
     }
-
 
     // Check Question 4
-    if (question4 === "") {
-        answerOutputEl4.innerHTML = "Required!";
-        questionInputEl4.style.border = "3px solid red";
+    if (question4 === "" ) {
+        answerEl4.innerHTML = "Required!";
+        questionEl4.style.border = "3px solid black";
     } else if (question4 === "1945") {
-        answerOutputEl1.innerHTML = "Correct!";
-        score ++;
+        answerEl4.innerHTML = "Correct!"
+        questionEl4.style.border = "3px solid green";
+        scoreF++;
     } else {
-        answerOutputEl1.innerHTML = "Incorrect!";
+        answerEl4.innerHTML = "Incorrect!"
+        questionEl4.style.border = "3px solid red";
     }
 
+    scoreP = (scoreF / 4) * 100;
 
-
+    if (scoreF === 1) {
+        scorefEl.innerHTML = scoreF + "/4";
+        scorepEl.innerHTML = scoreP + "% ";
+        feedbackEl.innerHTML = "At least you tried."
+    } else if (scoreF === 2) {
+        scorefEl.innerHTML = scoreF + "/4";
+        scorepEl.innerHTML = scoreP + "% ";
+        feedbackEl.innerHTML = "Not Bad."
+    } else if (scoreF === 3) {
+        scorefEl.innerHTML = scoreF + "/4";
+        scorepEl.innerHTML = scoreP + "% ";
+        feedbackEl.innerHTML = "There is always room for improvement."
+    } else {
+        scorefEl.innerHTML = scoreF + "/4";
+        scorepEl.innerHTML = scoreP + "% ";
+        feedbackEl.innerHTML = "Above and Beyond. Good Job.";
+    }
+    
     if (question1 === "" || question2 === "" || question3 === "" || question4 === "") {
-        missedQEl.style.display = "block";
-    } else {
-        scoreFractionEl.style.display = "block";
-        scorePercentageEl.style.display = "block";
-    }
-
-
+            missedQEl.style.display = "block";
+            scorefEl.style.display = "none";
+            scorepEl.style.display = "none";
+            feedbackEl.style.display = "none";
+        } else {
+            scorefEl.style.display = "block";
+            scorepEl.style.display = "block";
+            feedbackEl.style.display = "block";
+            missedQEl.style.display = "none";
+        }
+        
+    scoreF = 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // Check Question 1
+    // if (question1 = "") {
+    //     answerOutputEl2.innerHTML = "Required!";
+    // } else if (question1 === "usa" || question1 === "united states" || question1 === "united states of america") {
+    //     document.getElementById("flag.jpg").style.display = 'none';
+    //     answerOutputEl1.innerHTML = "Correct!";
+    //     score ++;
+    // } else {
+    //     answerOutputEl1.innerHTML = "Incorrect!";
+    // }
+
+    // // Check Question 2
+    // if (question2 === "") {
+    //     answerOutputEl1.innerHTML = "Required!";
+    // } else if (question2 === "ww2" || question1 === "world war 2" || question1 === "world war two") {
+    //     answerOutputEl1.innerHTML = "Correct!";
+    //     score ++;
+    // } else {
+    //     answerOutputEl1.innerHTML = "Incorrect!";
+    // }
+
+    // // Check Question 3
+    // if (question3 === "") {
+    //     answerOutputEl3.innerHTML = "Required!";
+    // } else if (question3 === "1") {
+    //     answerOutputEl1.innerHTML = "Correct!";
+    //     score ++;
+    // } else {
+    //     answerOutputEl1.innerHTML = "Incorrect!";
+    // }
+
+
+    // // Check Question 4
+    // if (question4 === "") {
+    //     answerOutputEl4.innerHTML = "Required!";
+    //     questionInputEl4.style.border = "3px solid red";
+    // } else if (question4 === "1945") {
+    //     answerOutputEl1.innerHTML = "Correct!";
+    //     score ++;
+    // } else {
+    //     answerOutputEl1.innerHTML = "Incorrect!";
+    // }
+
+
+
+    // if (question1 === "" || question2 === "" || question3 === "" || question4 === "") {
+    //     missedQEl.style.display = "block";
+    // } else {
+    //     scoreFractionEl.style.display = "block";
+    //     scorePercentageEl.style.display = "block";
+    // }
+
+
